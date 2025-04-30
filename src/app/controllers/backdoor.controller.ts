5b3aab6c2966f1066653c08dfb6f75d0e4c370be
max-shi <max.shi.1225@gmail.com>
2025-04-30 14:19:59 +1200

    test

--- src/app/controllers/backdoor.controller.ts
+++ src/app/controllers/backdoor.controller.ts
@@ -1,7 +1,7 @@
 import {Request, Response} from "express";
 import Logger from '../../config/logger';
 import * as Backdoor from '../models/backdoor.model';
-
+// test
 const resetDb = async (req: Request, res: Response):Promise<void> => {
     try {
         await Backdoor.resetDb();
